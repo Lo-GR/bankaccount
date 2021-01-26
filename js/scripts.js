8d5abebdcf8142db3c97493c994487c12fa90fe8
Lo-GR <diamondintheroth@gmail.com>
2021-01-26 15:58:12 -0800

    create business logic
    Woo Jin Kim <kimwoojin211@gmail.com>
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
+function Bank() {
+  this.accounts = {};
+  this.id = 0;
+}
+
+function Account(name, initialDeposit) {
+  this.name = name;
+  this.amount = initialDeposit;
+}
+
+Bank.prototype.assignId = function() {
+  this.id += 1;
+  return this.id;
+}
+
+Bank.prototype.addAccount = function(account) {
+  account.id = this.assignId();
+  this.accounts[account.id] = account;
+}
+
+
+Account.prototype.deposit = function(money) {
+  this.amount += money;
+}
+
+Account.prototype.withdraw = function(money) {
+  if(this.amount - money < 0) {
+    alert("Insufficient amount");
+  }
+  else {
+    this.amount -= money;
+  }
+}
+
+
+
+
+
+
 
 $(document).ready(function() {
 
